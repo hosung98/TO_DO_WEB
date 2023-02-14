86ba9a70d6b6586637d12a4cffee0155aa95711f
parkdaechul <eocjf4701@gmail.com>
2023-02-14 15:02:33 +0900

    로그인 테스트

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -9,17 +9,16 @@ const id = document.querySelector("#id"), //# = 태그의 id (ex) <input id= >
 loginBtn.addEventListener("click", login);
 
 function login() {
-
   let params = {
     id: id.value,
-    pw: password.value,
+    password: password.value,
   };
   
   if(!params.id) {
     alert("아이디를 입력하세요.");
     return;
   }
-  if(!params.pw) {
+  if(!params.password) {
     alert("비밀번호를 입력하세요.");
     return;
   }
@@ -29,21 +28,38 @@ function login() {
               .join('&');
   let url = 'http://127.0.0.1:3000/?' + query;
 
-  fetch(url)
-    .then(data => data.text())
-    .then((text) => {
-      console.log('request succeeded with JSON response', text);
-      if(text.length === 2) {
-        alert("아이디와 비밀번호가 일치하지 않습니다.");
-      }else {
-        alert("로그인 성공입니다.");
+  console.log(url);
+  fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(params),
+  })
+    .then((res) => res.json()) // json으로 읽는다.
+    .then((res) => {
+      if(res.success) {
         location.href = "/main";
+      }else {
+        alert(res.msg);
       };
+    })
+    .catch((err) => {
+      console.error(new Error("로그인 중 발생"));
+    })
 
-    }).catch(function (error) {
-      console.log('request failed', error)
-    });
+  // fetch(url)
+  //   .then(data => data.text())
+  //   .then((text) => {
+  //     console.log('request succeeded with JSON response', text);
+  //     if(text.length === 2) {
+  //       alert("아이디와 비밀번호가 일치하지 않습니다.");
+  //     }else {
+  //       alert("로그인 성공입니다.");
+  //       location.href = "/main";
+  //     };
+  //   }).catch(function (error) {
+  //     console.log('request failed', error)
+  //   });
 
-
-  //fetch();
 }
