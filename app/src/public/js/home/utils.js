f8a7d40002dd5c49eb0d0a5a6c8385ed37eaf3e3
chlvhksl <hwan4670@naver.com>
2023-02-22 15:52:38 +0900

    serverURL 체크 로직 수정

--- app/src/public/js/home/utils.js
+++ app/src/public/js/home/utils.js
@@ -2,7 +2,7 @@
 
 var serverUrl = document.location.href;
 
-if(serverUrl === "http://localhost:3001/") { // local
+if(serverUrl.indexOf("http://localhost:3001/") !== -1) { // local
   serverUrl = "http://localhost:3000"
 }else { // server
   serverUrl = "https://port-0-to-do-api-108dypx2ale7rwesa.sel3.cloudtype.app"
